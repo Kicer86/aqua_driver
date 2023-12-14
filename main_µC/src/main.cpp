7629007569c3d17816d5ab5ef54e893e388b10a0
Michał Walenciak <Kicer86@gmail.com>
2023-12-14 22:40:51 +0100

    Initial configuration

--- main_µC/src/main.cpp
+++ main_µC/src/main.cpp
@@ -1,5 +1,20 @@
 
+#include <avr/io.h>
+
+void setupUSART()
+{
+    int baud = 8;
+    /* Set baud rate */
+    UBRRH = (unsigned char)(baud>>8);
+    UBRRL = (unsigned char)baud;
+    /* Enable receiver and transmitter */
+    UCSRB = (1<<RXEN)|(1<<TXEN);
+    /* Set frame format: 8data, 2stop bit */
+    UCSRC = (1<<USBS)|(3<<UCSZ0);
+}
+
+
 int main()
 {
-
+    setupUSART();
 }
