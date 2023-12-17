c83b9b32af885aef2cfd28ec05a3843ac8deb26e
Michał Walenciak <Kicer86@gmail.com>
2023-12-17 14:57:03 +0100

    Setup UART for RN4871

--- main_µC/src/main.cpp
+++ main_µC/src/main.cpp
@@ -1,20 +1,44 @@
 
 #include <avr/io.h>
 
+#include "avr_lib/math.hpp"
+
+#ifndef F_CPU
+#error "F_CPU is not set"
+#endif
+
+
+template<long baud, bool speed_mode, int stop_bits>
 void setupUSART()
 {
-    int baud = 8;
-    /* Set baud rate */
-    UBRRH = (unsigned char)(baud>>8);
-    UBRRL = (unsigned char)baud;
-    /* Enable receiver and transmitter */
-    UCSRB = (1<<RXEN)|(1<<TXEN);
-    /* Set frame format: 8data, 2stop bit */
-    UCSRC = (1<<USBS)|(3<<UCSZ0);
+    static_assert(stop_bits == 1 || stop_bits == 2);
+    constexpr int mul = speed_mode? 8 : 16;
+
+    constexpr double exact_ubrr = static_cast<double>(F_CPU) / (mul * baud) - 1;
+    constexpr long rounded_ubrr = round<long>(exact_ubrr);
+    static_assert(rounded_ubrr < 65536);
+
+    constexpr long  real_baud = F_CPU / (mul * (rounded_ubrr + 1));
+    constexpr long diff = abs(real_baud - baud);
+    constexpr auto error = diff/static_cast<double>(baud) * 100;
+
+    // based on https://stackoverflow.com/questions/65092264/how-to-set-atmega64a-au-usart-speed-to-115200
+    static_assert(error <= 4.5, "Too big frequency error for USART. Adjust F_CPU and/or baud rate");
+
+    // setup usart module
+    UBRRH = (baud >> 8) & 0xff;
+    UBRRL = baud & 0xff;
+
+    UCSRB = (1 << RXEN) | (1 << TXEN);
+
+    UCSRC = (3<<UCSZ0);
+    if (stop_bits == 2)
+        UCSRC |= (1<<USBS);
 }
 
 
 int main()
 {
-    setupUSART();
+    setupUSART<115200, true, 1>();
+    while(true);
 }
