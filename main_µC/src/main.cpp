d0fe1122feebe744c468027d88e9c0f6ecf95296
Michał Walenciak <Kicer86@gmail.com>
2023-12-23 09:14:26 +0100

    Use avr-libstdcpp and fully switch to avr-lib's Usart

--- main_µC/src/main.cpp
+++ main_µC/src/main.cpp
@@ -4,46 +4,11 @@
 #include "avr-lib/include/math.hpp"
 #include "avr-lib/include/devices/usart.hpp"
 
-#ifndef F_CPU
-#error "F_CPU is not set"
-#endif
-
-
-template<long baud, bool speed_mode, int stop_bits>
-void setupUSART()
-{
-    static_assert(stop_bits == 1 || stop_bits == 2);
-    constexpr int mul = speed_mode? 8 : 16;
-
-    constexpr double exact_ubrr = static_cast<double>(F_CPU) / (mul * baud) - 1;
-    constexpr long rounded_ubrr = round<long>(exact_ubrr);
-    static_assert(rounded_ubrr < 65536);
-
-    constexpr long  real_baud = F_CPU / (mul * (rounded_ubrr + 1));
-    constexpr long diff = abs(real_baud - baud);
-    constexpr auto error = diff/static_cast<double>(baud) * 100;
-
-    // based on https://stackoverflow.com/questions/65092264/how-to-set-atmega64a-au-usart-speed-to-115200
-    static_assert(error <= 4.5, "Too big frequency error for USART. Adjust F_CPU and/or baud rate");
-
-    // setup usart module
-    UBRRH = (baud >> 8) & 0xff;
-    UBRRL = baud & 0xff;
-
-    UCSRB = (1 << RXEN) | (1 << TXEN);
-
-    UCSRC = (3<<UCSZ0);
-    if (stop_bits == 2)
-        UCSRC |= (1<<USBS);
-}
-
 
 int main()
 {
-    //setupUSART<115200, true, 1>();
-
     Usart usart;
-    usart.configure(true, true, Baudrate::BR115200, Usart::None, Usart::S1, Usart::D8);
+    usart.configure<true, true, 115200, Usart::None, Usart::S1, Usart::D8>();
 
     while(true);
 }
